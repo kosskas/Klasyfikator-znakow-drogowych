77e396e3960935133b4de63840c6e398e460d651
kosskas <93159483+kosskas@users.noreply.github.com>
2023-05-03 23:44:39 +0200

    Pomiary Upd
    Dodano kilka pomiarów

--- main.py
+++ main.py
@@ -7,20 +7,19 @@ from kNN import NearestNeighborClasiffier
 from Linear import LinearClasiffier
 from load_data import load_data, get_classes_num
 
-
+###
+load_data.N_OF_CLASSES = 3 # 36
+##
 print(f"liczba klas = {get_classes_num()}")
 print("wczytywanie")
 train_data, test_data = load_data()
 print("start")
 #model = NearestNeighborClasiffier(norma = 1)
-model = LinearClasiffier(klasy=get_classes_num(), metoda="localsearch",iters=10000,step=0.00001)
-model.wczytaj_model("36_100000")
+#model = LinearClasiffier(klasy=get_classes_num(), metoda="localsearch",iters=1000,step=0.0001)
+##model.wczytaj_model("rr")
 #model.train(*train_data)
-model.evaluate(*test_data)
-#model.zapisz_model("n1")
-
-
-
+#model.evaluate(*test_data)
+#model.zapisz_model("36_1000")
 #
 #i, ls = model.get_loss()
 #plt.plot(i, ls)
@@ -29,21 +28,27 @@ model.evaluate(*test_data)
 #plt.ylabel("Funkcja straty")
 #plt.show()
 
-#for j in [-5, -6, -7]:
-#    step = 10**j
-#    model = LinearClasiffier(metoda="localsearch",iters=10000,step=step)
-#    model.train(*train_data)
-#    i, ls = model.get_loss()
-#    sre = model.evaluate(*test_data)
-#    plt.plot(i, ls, label = j)
-#plt.legend(loc="upper right")
-#plt.show()
+###############
+
+for j in [-5, -6, -7]:
+    step = 10**j
+    print(f"LR = {10**j}")
+    model = LinearClasiffier(klasy=get_classes_num(),metoda="localsearch",iters=10000,step=step)
+    model.train(*train_data)
+    i, ls = model.get_loss()
+    model.evaluate(*test_data)
+    plt.plot(i, ls, label = f"LR = {10**j}")
+plt.legend(loc="upper right")
+plt.title("Wartość funkcji straty w kolejnych iteracjach")
+plt.xlabel("Iteracje")
+plt.ylabel("Funkcja straty")
+plt.show()
 
 
 
 
 
-
+##KNN###
 ##najlepsze k crosswalidacja
 #valid = []
 #Xtr, Ytr = train_data
