d1135c99704c44d786433e3e2b7ad2c93c8e1e90
kosskas <93159483+kosskas@users.noreply.github.com>
2023-05-05 09:36:58 +0200

    Test2

--- Linear.py
+++ Linear.py
@@ -55,10 +55,10 @@ class LinearClasiffier:
         self.strata = []
         for i in range(self.iters):
             step_size = self.step
-            Wtry = self.W + np.random.randn(self.__CLASS_NUM__, 3073) * step_size
-            loss = L(Xtrain, Ytrain, Wtry)
+            Wnew = self.W + np.random.randn(self.__CLASS_NUM__, 3073) * step_size
+            loss = L(Xtrain, Ytrain, Wnew)
             if loss < bestloss:
-                self.W = Wtry
+                self.W = Wnew
                 bestloss = loss            
             self.licz.append(i)
             self.strata.append(loss)
@@ -69,11 +69,11 @@ class LinearClasiffier:
         #raz lepiej raz gorzej
         bestloss = float("inf")
         for i in range(self.iters):        
-            W_iter = np.random.randn(self.__CLASS_NUM__, 3073) * self.step
-            loss = L(Xtrain, Ytrain, W_iter) 
+            Wnew = np.random.randn(self.__CLASS_NUM__, 3073) * self.step
+            loss = L(Xtrain, Ytrain, Wnew) 
             if loss < bestloss:
                 bestloss = loss
-                self.W = W_iter
+                self.W = Wnew
             if i % 100 == 0:
                 print('iteracja %d the loss was %f, best %f' % (i, loss, bestloss))
 
@@ -100,55 +100,3 @@ def L(X, y, W):
     margins[y, np.arange(X.shape[1])] = 0
     loss = np.sum(margins) / X.shape[1]
     return loss
-
-
-#########################
-#def L_i(x, y, W):
-#    """
-#    unvectorized version. Compute the multiclass svm loss for a single example (x,y)
-#    - x is a column vector representing an image (e.g. 3073 x 1 in CIFAR-10)
-#    with an appended bias dimension in the 3073-rd position (i.e. bias trick)
-#    - y is an integer giving index of correct class (e.g. between 0 and 9 in CIFAR-10)
-#    - W is the weight matrix (e.g. 10 x 3073 in CIFAR-10)
-#    """
-#    delta = 1.0 # see notes about delta later in this section
-#    scores = W.dot(x) # scores becomes of size 10 x 1, the scores for each class
-#    correct_class_score = scores[y]
-#    D = W.shape[0] # number of classes, e.g. 10
-#    loss_i = 0.0
-#    for j in range(D): # iterate over all wrong classes
-#        if j == y:
-#        # skip for the true class to only loop over incorrect classes
-#            continue
-#        # accumulate loss for the i-th example
-#        loss_i += max(0, scores[j] - correct_class_score + delta)
-#    return loss_i
-
-#def L_i_vectorized(x, y, W):
-#    """
-#    A faster half-vectorized implementation. half-vectorized
-#    refers to the fact that for a single example the implementation contains
-#    no for loops, but there is still one loop over the examples (outside this function)
-#    """
-#    delta = 1.0
-#    scores = W.dot(x)
-#    # compute the margins for all classes in one vector operation
-#    margins = np.maximum(0, scores - scores[y] + delta)
-#    # on y-th position scores[y] - scores[y] canceled and gave delta. We want
-#    # to ignore the y-th position and only consider margin on max wrong class
-#    margins[y] = 0
-#    loss_i = np.sum(margins)
-#    return loss_i
-
-#def foll_gradient(self, Xtrain, Ytrain):
-#        """ cos z gradientem, nie wiem co to jest i długo liczy
-#        """
-#        self.W = np.random.rand(3, 3073) * self.step
-#        loss_original = self.loss_fun(self.W) # the original loss
-#        print('original loss: %f' % (loss_original, ))
-#        # lets see the effect of multiple step sizes
-#        bestloss = float("inf")
-#        for i in range(self.iters):
-#            W_new = self.W - step_size * df # new position in the weight space
-#            loss_new = self.loss_fun(W_new)
-#            print( 'for step size %f new loss: %f' % (step_size, loss_new))
