c688e4e696e07b48a276ef0f3a51900e8eef6ff1
Tomasz Szcząchor <89033748+tomSzczach@users.noreply.github.com>
2023-05-20 22:38:07 +0200

    Added model choosing to GUI

--- gui.py
+++ gui.py
@@ -1,13 +1,17 @@
 # Import customtkinter module
+import tkinter
 from tkinter import filedialog
 
 import customtkinter as ctk
+import tensorflow as tf
 from PIL import Image
 
 import os, sys, csv, cv2, random
 import numpy as np
 from Linear import LinearClasiffier
-from load_data import load_data, get_classes_num
+from kNN import NearestNeighborClasiffier
+from load_data import load_data, get_classes_num, load_images
+
 load_data.N_OF_CLASSES = 36 # 36
 
 
@@ -67,26 +71,51 @@ ctk.set_appearance_mode("System")
 ctk.set_default_color_theme("green")
 
 # defines
-window_width = 500
-window_height = 500
+window_width = 600
+window_height = 600
 
+# linear_classifier
+print("loading linear_classifier_model...")
 linear_classifier_model = LinearClasiffier(klasy=get_classes_num(), metoda="localsearch", iters=10000, step=0.00001)
-linear_classifier_model.wczytaj_model("./Modele/model_36_100000")
+linear_classifier_model.wczytaj_model("Modele/model_36_100000")
+print("done")
+
+# convolutional_network
+print("loading convolutional_network_model...")
+convolutional_network_model = tf.keras.models.load_model("Modele/nsiec36")
+print("done")
+
+# kNN
+print("loading kNN_model...")
+x_train, y_train = load_images("train.csv")
+kNN_model = NearestNeighborClasiffier(klasy=get_classes_num(), norma=2, k=1)
+kNN_model.train(x_train, y_train)
+print("done")
 
 
-def image_to_binary_array(image_path):
+def image_to_binary_array(model, image_path):
     pixels = []
     image = cv2.imread(image_path)
     image = cv2.resize(image, (32, 32))
-    pixels.append(np.array(image).flatten())
+
+    if model == "linear_classifier":
+        pixels.append(np.array(image).flatten())
+    elif model == "convolutional_network":
+        pixels.append(np.array(image))
+    elif model == "kNN":
+        pixels.append(np.array(image).flatten())
+
     return np.array(pixels)
 
 
 def predicate(model, image_path):
-    test = image_to_binary_array(image_path)
-    # prd_idx = int(model.predict(test))
-    # print(f"Znak {klasa[nadrzedna(prd_idx)]}\nOpis: {opis[prd_idx]}")
-    return int(model.predict(test))
+    test = image_to_binary_array(model, image_path)
+    if model == "linear_classifier":
+        return int(linear_classifier_model.predict(test))
+    elif model == "convolutional_network":
+        return int(np.argmax(convolutional_network_model.predict(test), axis=-1))
+    elif model == "kNN":
+        return int(kNN_model.predict(test))
 
 
 class App(ctk.CTk):
@@ -97,16 +126,40 @@ class App(ctk.CTk):
         self.geometry(f"{window_width}x{window_height}")
 
         # create 2x2 grid system
-        self.grid_rowconfigure((0, 1, 2), weight=1)
-        self.grid_columnconfigure((0, 1, 2, 3, 4), weight=1)
-
-        self.chooseImageButton = ctk.CTkButton(self, command=self.choose_image,
-                                               text="Wybierz znak", width=120, height=40)
-        self.chooseImageButton.grid(row=1, column=1)
+        self.grid_rowconfigure((0, 1), weight=1)
+        self.grid_columnconfigure((0, 1, 2, 3), weight=1)
 
         self.image_path = None
-        self.image_label = None
-        self.answer_textbox = None
+        self.chosen_image_label = None
+
+        # answer image
+        self.answer_image_label = ctk.CTkLabel(self, text="", fg_color="transparent")
+
+        # answer textbox
+        self.answer_textbox = ctk.CTkTextbox(master=self, height=100, width=250, corner_radius=3)
+        self.answer_textbox.grid(row=1, column=1)
+
+        # frame
+        self.frame = ctk.CTkFrame(master=self)
+        self.frame.grid(row=0, column=0)
+
+        self.chosen_model = tkinter.StringVar(value="none")
+
+        self.linear_button = ctk.CTkRadioButton(self.frame, variable=self.chosen_model, command=self.show_answer,
+                                                  value="linear_classifier", text="klasyfikator liniowy")
+        self.linear_button.grid(row=0, column=0, sticky="w", padx=10, pady=10)
+
+        self.convolutional_button = ctk.CTkRadioButton(self.frame, variable=self.chosen_model, command=self.show_answer,
+                                                    value="convolutional_network", text="sieć konwolucyjna")
+        self.convolutional_button.grid(row=1, column=0, sticky="w", padx=10, pady=10)
+
+        self.kNN_button = ctk.CTkRadioButton(self.frame, variable=self.chosen_model, command=self.show_answer,
+                                                  value="kNN", text="kNN")
+        self.kNN_button.grid(row=2, column=0, sticky="w", padx=10, pady=10)
+
+        self.chooseImageButton = ctk.CTkButton(self.frame, command=self.choose_image,
+                                               text="Wybierz znak", width=120, height=40)
+        self.chooseImageButton.grid(row=3, column=0, padx=10, pady=20)
 
     def choose_image(self):
         self.image_path = filedialog.askopenfilename(
@@ -114,56 +167,35 @@ class App(ctk.CTk):
             title="Choose a photo",
             filetypes=(("png", "*.png"), ("jpg", "*.jpg"))
         )
-        self.show_image(self.image_path, 2, 1, 150, 150, "")
-        self.show_answer(self.image_path, 1, 2, 100, 250)
+        self.show_image(self.chosen_image_label, self.image_path, 0, 1, 150, 150, "")
+        # reset
+        self.chosen_model.set(value="none")
+        self.show_image(self.answer_image_label, None, 1, 0, 150, 150, "")
+        self.answer_textbox.delete("0.0", "end")
 
-    def show_image(self, image_path, row, col, h, w, text):
-        self.image_label = ctk.CTkLabel(self,
-                                        image=ctk.CTkImage(
+    def show_image(self, image_label_ref, image_path, row, col, h, w, text):
+        if image_path is None:
+            image_path = f".\\Meta\\none.png"
+
+        image_label_ref = ctk.CTkLabel(self,
+                                       image=ctk.CTkImage(
                                             light_image=Image.open(image_path),
                                             dark_image=Image.open(image_path),
-                                            size=(h, w)),
-                                        text=text
-                                        )
-        self.image_label.grid(row=row, column=col)
+                                            size=(h, w)
+                                       ),
+                                       text=text)
+        image_label_ref.grid(row=row, column=col)
 
-    def show_answer(self, image_path, row, col, h, w):
-        idx = predicate(linear_classifier_model, image_path)
-        self.answer_textbox = ctk.CTkTextbox(master=self, height=h, width=w, corner_radius=3)
-        self.answer_textbox.grid(row=row, column=col)
-        self.answer_textbox.insert("0.0", f"Znak {sign_class(idx)}\n\n{sign_desc[idx]}")
-        self.show_image(f".\\Meta\\{idx}.png", 2, 2, 150, 150, "")
+    def show_answer(self):
+        if self.chosen_model.get() == "none" or self.image_path is None:
+            return
+        answer = predicate(self.chosen_model.get(), self.image_path)
+        self.show_image(self.answer_image_label, f".\\Meta\\{answer}.png", 1, 0, 150, 150, "")
+        self.answer_textbox.delete("0.0", "end")
+        self.answer_textbox.insert("0.0", f"Znak {sign_class(answer)} \n\n {sign_desc[answer]}")
 
 
 if __name__ == "__main__":
     app = App()
     # Runs the app
     app.mainloop()
-
-
-# from tkinter import *
-# from PIL import ImageTk,Image
-# from tkinter import filedialog
-#
-#
-# root = Tk()
-# root.title('SI GUI')
-#
-#
-# def open():
-#     global image
-#     root.filename = filedialog.askopenfilename(
-#         initialdir="./zdjTestowe",
-#         title="Choose a photo",
-#         filetypes=(("png", "*.png"), ("jpg", "*.jpg"))
-#     )
-#     image = ImageTk.PhotoImage(Image.open(root.filename))
-#     Label(image=image).pack()
-#
-#
-# Button(root, text="Open", command=open).pack()
-#
-#
-# root.mainloop()
-
-
